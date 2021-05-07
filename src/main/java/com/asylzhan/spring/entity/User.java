1ebc07b71a3fa9af76b6c3ff9bd3c40c56c7e7db
AsylzhanUbaidullaev <asylzhan18092001@gmail.com>
2021-05-07 19:25:59 +0600

    endterm.

--- src/main/java/com/asylzhan/spring/entity/User.java
+++ src/main/java/com/asylzhan/spring/entity/User.java
@@ -32,6 +32,7 @@ public class User implements UserDetails {
     )
     private List<Role> roles;
 
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return roles;
